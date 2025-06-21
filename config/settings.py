981fe1ff97ccdd2af510eb2c5b78126b71ee7024
Haohanmaiyami <kharitonovayan2018@gmail.com>
2025-06-21 02:33:05 -0400

    first commit

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
-
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from pathlib import Path
 
 
@@ -62,9 +63,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
